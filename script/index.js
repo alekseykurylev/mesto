e510a526ba381c1daf8f1818d68f44ddc9628144
ALEKSEY KURYLEV <alekseykurylev@MacBook-Air-79875375880.local>
2020-12-21 07:57:48 +0300

    Первый коммит

---
+++ script/index.js
@@ -0,0 +1,32 @@
+let editProfile = document.querySelector('.profile__button-edit')
+let popup = document.querySelector('.popup')
+let closePopup = document.querySelector('.popup__close')
+let formElement = popup.querySelector('.form')
+let nameInput = formElement.querySelector('.name-input')
+let jobInput = formElement.querySelector('.job-input')
+let nameProfile = document.querySelector('.profile__name')
+let jobProfile = document.querySelector('.profile__job')
+
+function togglePopup() {
+    popup.classList.toggle('popup_opened')
+    if(popup.classList.contains('popup_opened')) {
+        nameInput.value = nameProfile.textContent
+        jobInput.value = jobProfile.textContent
+    }
+}
+
+editProfile.addEventListener('click', togglePopup)
+closePopup.addEventListener('click', togglePopup)
+
+function handleFormSubmit(evt) {
+    evt.preventDefault();
+    
+    nameProfile.textContent = nameInput.value
+    jobProfile.textContent = jobInput.value
+
+    togglePopup()
+
+}
+
+formElement.addEventListener('submit', handleFormSubmit);
+
